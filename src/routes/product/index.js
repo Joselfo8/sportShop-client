672699e0d958eb2f5bafc127a17ba7ba2df035ef
Hans Garcia <hansvekoni@gmail.com>
2022-07-05 20:21:23 -0300

    se creo la ruta del post

--- src/routes/product/index.js
+++ src/routes/product/index.js
@@ -1,12 +1,40 @@
 const router = require("express").Router();
 const { newProduct } = require("../handleDB");
 const { getProductByName, getDBproducts } = require("./function");
+import Category from "../../models/Category";
+import Product from "../../models/Product";
 
 router.get("", (req, res) => {
   newProduct("producto1", 1, "descripcion1").then(() => {
     res.send("ya se creo el producto");
   });
 });
+//post /Product
+const postProduct = async (req, res, next) => {
+  const { title, price, description, categoryName } = req.body;
+  if (!title.length > 0 || !title.length < 25 || !title.char[0] !== " " || !categoryName) {
+    res.status(400).send("there is not product with that name");
+  }
+  else {
+    try {
+      const productCreated = await Product.create({
+        title,
+        price,
+        description
+      })
+      const category = await Category.findOne({
+        where: {
+          name,
+        }
+      })
+    }
+    catch (error) {
+      next(error);
+      res.send(error)
+    }
+  }
+}
+
 //GET ALL PRODUCTS
 const getAllProducts = async (req, res, next) => {
   //res.send('<h1>get all products</h1>')
@@ -35,5 +63,7 @@ const getByName = async (req, res, next) => {
     next(err);
   }
 };
-
+router.get("/:name", getByName);
+router.get("", getAllProducts);
+router.post("", postProduct);
 module.exports = { products: router };
