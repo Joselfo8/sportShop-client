bf08581eb56c83ee6a383349dc1b0f5a8e23514e
Jose <joseluisfo_@hotmail.com>
2022-07-05 21:36:16 -0500

    Navbar primer commit

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+
+export default function NavBar(){
+    const [value, setValue] = useState({})
+
+
+    function handleChange(event: any) {
+        setValue(event)
+    }
+
+    function handleSubmit(event: any) {
+        event.preventDefault();
+        console.log(value)
+    }
+
+    return (
+        <div>
+            
+            <form onSubmit={(e) => handleSubmit(e)}>
+                <input 
+                    type="text" 
+                    name="searchProducts"
+                    id="searchProducts"
+                    placeholder='Search by products...' 
+                    onChange={(e) => handleChange(e.target.value)} 
+                />
+                <button type="submit">Search</button>
+            </form>
+            
+            <button>Hombre</button>
+
+            <button>Mujer</button>
+
+            <button>Infantil</button>
+
+            <button>Deporte</button>
+
+        </div>
+    );
+};
