9493f8340ef40dfd82aa100714bc42eff0115785
Andresser17 <tensasangetsu1254@gmail.com>
2022-07-06 18:32:09 -0400

    feat: add SignUp and SignIn components in Login.tsx
    add title and submit button to form

--- src/routes/Login.tsx
+++ src/routes/Login.tsx
@@ -9,21 +9,25 @@ function SignUp() {}
 function SignIn() {
   return (
     <>
-      <Input
-        text="Email"
-        id="email"
-        // required={undefined}
-        // placeholder={undefined}
-        getData={() => {}}
-      />
+      <Input text="Email" id="email" getData={() => {}} />
+      <Input text="Password" id="password" getData={() => {}} />
+      <span className={styles["subtitle"]}>Forgot your password?</span>
     </>
   );
 }
 
 function Login() {
   return (
-    <div className={styles["container"]}>
-      <SignIn />
+    <div className={`${styles["body"]} secondary`}>
+      <form className={styles["container"]}>
+        <span className={styles["title"]}>Login</span>
+        <SignIn />
+        <div className={styles["button-cont"]}>
+          <button className={`${styles["submit-button"]} primary`}>
+            Sign In
+          </button>
+        </div>
+      </form>
     </div>
   );
 }
