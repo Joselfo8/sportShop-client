c9e39e71add7e3d2c3346325e1939297b81babb0
Andresser17 <tensasangetsu1254@gmail.com>
2022-07-06 19:09:17 -0400

    feat: add icons to Login.tsx
    create icons folder with svg icon files

--- src/routes/Login.tsx
+++ src/routes/Login.tsx
@@ -1,17 +1,36 @@
 import { useState } from "react";
 // Components
 import Input from "../components/Input";
+// Icons
+import { ReactComponent as FacebookIcon } from "../icons/facebook-icon.svg";
+import { ReactComponent as GoogleIcon } from "../icons/google-icon.svg";
+import { ReactComponent as LinkedinIcon } from "../icons/linkedin-icon.svg";
 // Styles
 import styles from "./Login.module.css";
+
+function AuthLogin() {
+  return (
+    <div className={styles["social-media"]}>
+      <span className={styles["or-subtitle"]}>Or login with</span>
+      <FacebookIcon className={styles["social-media-icon"]} />
+      <GoogleIcon className={styles["social-media-icon"]} />
+      <LinkedinIcon className={styles["social-media-icon"]} />
+    </div>
+  );
+}
 
 function SignUp() {}
 
 function SignIn() {
   return (
     <>
-      <Input text="Email" id="email" getData={() => {}} />
-      <Input text="Password" id="password" getData={() => {}} />
-      <span className={styles["subtitle"]}>Forgot your password?</span>
+      <div className={styles["input-wrapper"]}>
+        <Input text="Email" id="email" getData={() => {}} />
+      </div>
+      <div className={styles["input-wrapper"]}>
+        <Input text="Password" id="password" getData={() => {}} />
+        <span className={styles["subtitle"]}>Forgot your password?</span>
+      </div>
     </>
   );
 }
@@ -19,15 +38,18 @@ function SignIn() {
 function Login() {
   return (
     <div className={`${styles["body"]} secondary`}>
-      <form className={styles["container"]}>
-        <span className={styles["title"]}>Login</span>
-        <SignIn />
-        <div className={styles["button-cont"]}>
-          <button className={`${styles["submit-button"]} primary`}>
-            Sign In
-          </button>
-        </div>
-      </form>
+      <div className={styles["container"]}>
+        <form>
+          <span className={styles["title"]}>Login</span>
+          <SignIn />
+          <div className={styles["button-cont"]}>
+            <button className={`${styles["submit-button"]} primary`}>
+              Sign In
+            </button>
+          </div>
+        </form>
+        <AuthLogin />
+      </div>
     </div>
   );
 }
