e86312e1318cba0bd2823bc833dd922373145650
Andresser17 <tensasangetsu1254@gmail.com>
2022-07-08 14:59:55 -0400

    feat: add UserProfile component

---
+++ src/routes/UserProfile.tsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+// Components
+import Tabs from "../components/Tabs";
+// Styles
+import styles from "./Login.module.css";
+
+interface UserProfileProps {
+  register?: boolean;
+}
+
+function UserProfile() {
+  return (
+    <div className={`${styles["body"]} secondary`}>
+    </div>
+  );
+}
+
+export default UserProfile;
