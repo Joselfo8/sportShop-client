623b9ece942942e5c2835d744524d3ce7592a379
Andresser17 <tensasangetsu1254@gmail.com>
2022-07-08 16:39:05 -0400

    feat: add UserProfile component
    display favorite products, orders, user and shipping information

--- src/routes/UserProfile.tsx
+++ src/routes/UserProfile.tsx
@@ -1,16 +1,45 @@
 import { useState } from "react";
 // Components
 import Tabs from "../components/Tabs";
+// Icons
+import { ReactComponent as DefaultUser } from "../icons/default-user.svg";
 // Styles
-import styles from "./Login.module.css";
+import styles from "./UserProfile.module.css";
 
-interface UserProfileProps {
-  register?: boolean;
+function Sidebar({ getSelected }: { getSelected: (prev: string) => void }) {
+  return (
+    <div className={`${styles["sidebar"]} dark`}>
+      <div className={styles["user-image-cont"]}>
+        <DefaultUser className={styles["user-image"]} />
+        {/* <img */}
+        {/*   className={styles["user-image"]} */}
+        {/*   src={} */}
+        {/*   alt="user-image" */}
+        {/* /> */}
+      </div>
+      <div className={styles["sidebar-body"]}>
+        <Tabs
+          tabs={["User information", "My orders", "My favorites"]}
+          getSelected={getSelected}
+        />
+      </div>
+    </div>
+  );
+}
+
+function ListContainer() {
+  return <div className={`${styles["list-container"]} secondary`}></div>;
 }
 
 function UserProfile() {
+  const [selectedTab, setSelectedTab] = useState("");
+
   return (
     <div className={`${styles["body"]} secondary`}>
+      <div className={styles["container"]}>
+        <Sidebar getSelected={setSelectedTab} />
+        <ListContainer />
+      </div>
     </div>
   );
 }
