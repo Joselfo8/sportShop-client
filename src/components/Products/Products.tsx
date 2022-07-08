3a3151428ea3815cb60aa7ebbe29baa35a81d04a
Andresser17 <tensasangetsu1254@gmail.com>
2022-07-08 11:55:40 -0400

    feat: add Pagination component to Products route

--- src/components/Products/Products.tsx
+++ src/components/Products/Products.tsx
@@ -1,58 +1,77 @@
-import React, {useEffect} from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { getAllJSDocTagsOfKind } from 'typescript';
-import { getProducts } from '../../redux/action';
-import Card from '../Card/Card';
-import NavBar from '../Navbar/Navbar';
-import style from './Products.module.scss'
+import { useState, useEffect } from "react";
+// import { getAllJSDocTagsOfKind } from "typescript";
+// Store
+import { useDispatch, useSelector } from "react-redux";
+import { getProducts } from "../../redux/action";
+// Components
+import Card from "../Card/Card";
+import Pagination from "../Pagination";
+// import NavBar from "../Navbar/Navbar";
+import style from "./Products.module.scss";
 
 export default function Products() {
-
+    // save user click pagination button
+    const [selected, setSelected] = useState(1);
     const dispatch = useDispatch();
-    const state = useSelector((state: any) => state); 
+    const state = useSelector((state: any) => state);
 
+    // get products from store
     useEffect(() => {
-        dispatch(getProducts())
-    },[dispatch]);
+        dispatch(getProducts());
+    }, [dispatch]);
 
     const render = {
-        allProducts: 
-            state.products.length === 0
-            ?   <div>
+        allProducts:
+            state.products.length === 0 ? (
+                <div>
                     <h2>Loading products</h2>
                 </div>
-            : state.products.map((p: any) => {
-                return <Card 
+            ) : (
+                state.products.map((p: any) => {
+                    return (
+                        <Card
                             key={p.title}
                             id={p.id}
                             image={p.image}
                             title={p.title}
                             price={p.price}
                         />
-            }),
+                    );
+                })
+            ),
 
         searchProducts:
-            state.productsFiltered.length === 0
-            ?   <div>
+            state.productsFiltered.length === 0 ? (
+                <div>
                     <h2>No products found!</h2>
                 </div>
-            : state.productsFiltered.map((p: any) => {
-                return <Card 
+            ) : (
+                state.productsFiltered.map((p: any) => {
+                    return (
+                        <Card
                             key={p.title}
                             id={p.id}
                             image={p.image}
                             title={p.title}
                             price={p.price}
                         />
-            })  
-        }
-    
+                    );
+                })
+            ),
+    };
 
     return (
         <div className={style.cardContainer}>
-            {
-                state.productsFiltered.length === 0 ? render.allProducts : render.searchProducts
-            }
+            {state.productsFiltered.length === 0
+                ? render.allProducts
+                : render.searchProducts}
+            <Pagination
+                maxPage={state.products.length}
+                next={{ limit: 10, page: 2 }}
+                previous={{ limit: 10, page: 1 }}
+                selected={selected}
+                onSelected={setSelected}
+            />
         </div>
     );
-};
+}
