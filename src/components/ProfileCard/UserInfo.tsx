ccb652bbb5ef4c19aef09e5d16ed71940e929798
Andresser17 <tensasangetsu1254@gmail.com>
2022-07-09 10:59:52 -0400

    feat(UserInfo): add Shipping and ShippingCont components

--- src/components/ProfileCard/UserInfo.tsx
+++ src/components/ProfileCard/UserInfo.tsx
@@ -15,7 +15,7 @@ function UpdateForm() {
   );
 }
 
-interface ShippingAddressProps {
+interface AddressProps {
   name: string;
   address: string;
   secondAddress?: string;
@@ -25,7 +25,7 @@ interface ShippingAddressProps {
   phone: string;
 }
 
-function ShippingAddress({
+function Address({
   name,
   address,
   secondAddress,
@@ -33,99 +33,113 @@ function ShippingAddress({
   state,
   zipCode,
   phone,
-}: ShippingAddressProps) {
+}: AddressProps) {
   return (
     <li className={`${styles["ship-li"]} primary`}>
       <span>{name}</span>
       <span>{address}</span>
       <span>{secondAddress}</span>
-      <span>{city} {state} {zipCode}</span>
+      <span>
+        {city} {state} {zipCode}
+      </span>
       <span>{phone}</span>
     </li>
+  );
+}
+
+function AddressCont({ edit }: { edit: boolean }) {
+  return (
+    <div className={styles["container"]}>
+      <div className={styles["title-cont"]}>
+        <span className={styles["title"]}>Shipping addresses</span>
+        <span className={styles["edit-button"]}>Edit</span>
+      </div>
+      <form className={styles["shipping-form"]} action="#">
+        <Address
+          name="John Doe"
+          address="Plaza Commerce St 172"
+          city="Dallas"
+          state="Texas"
+          zipCode={33172}
+          phone="555-01023021"
+        />
+
+        <Address
+          name="John Doe"
+          address="Plaza Commerce St 172"
+          city="Dallas"
+          state="Texas"
+          zipCode={33172}
+          phone="555-01023021"
+        />
+
+        <Address
+          name="John Doe"
+          address="Plaza Commerce St 172"
+          city="Dallas"
+          state="Texas"
+          zipCode={33172}
+          phone="555-01023021"
+        />
+
+        <Address
+          name="John Doe"
+          address="Plaza Commerce St 172"
+          city="Dallas"
+          state="Texas"
+          zipCode={33172}
+          phone="555-01023021"
+        />
+      </form>
+      {edit && <UpdateForm />}
+    </div>
+  );
+}
+
+function PersonalInfo({ edit }: { edit: boolean }) {
+  return (
+    <div className={styles["container"]}>
+      <div className={styles["title-cont"]}>
+        <span className={styles["title"]}>User information</span>
+        <span className={styles["edit-button"]}>Edit</span>
+      </div>
+
+      <form className={styles["form"]} action="#">
+        <Input
+          text="Name"
+          placeholder="First name"
+          id="name"
+          getData={(data: string) => console.log(data)}
+        />
+        <Input
+          text="Last name"
+          placeholder="Last name"
+          id="name"
+          getData={(data: string) => console.log(data)}
+        />
+        <Input
+          text="Username"
+          placeholder="Username"
+          id="username"
+          getData={(data: string) => console.log(data)}
+        />
+        <Input
+          text="Email"
+          placeholder="Email"
+          id="email"
+          getData={(data: string) => console.log(data)}
+        />
+        {edit && <UpdateForm />}
+      </form>
+    </div>
   );
 }
 
 function UserInfo() {
   return (
     <>
-      <div className={styles["container"]}>
-        <div className={styles["title-cont"]}>
-          <span className={styles["title"]}>User information</span>
-          <span className={styles["edit-button"]}>Edit</span>
-        </div>
-
-        <form className={styles["form"]} action="#">
-          <Input
-            text="Name"
-            placeholder="First name"
-            id="name"
-            getData={(data: string) => console.log(data)}
-          />
-          <Input
-            text="Last name"
-            placeholder="Last name"
-            id="name"
-            getData={(data: string) => console.log(data)}
-          />
-          <Input
-            text="Username"
-            placeholder="Username"
-            id="username"
-            getData={(data: string) => console.log(data)}
-          />
-          <Input
-            text="Email"
-            placeholder="Email"
-            id="email"
-            getData={(data: string) => console.log(data)}
-          />
-          <UpdateForm />
-        </form>
-      </div>
-      <div className={styles["container"]}>
-        <div className={styles["title-cont"]}>
-          <span className={styles["title"]}>Shipping addresses</span>
-          <span className={styles["edit-button"]}>Edit</span>
-        </div>
-        <form className={styles["shipping-form"]} action="#">
-          <ShippingAddress
-            name="John Doe"
-            address="Plaza Commerce St 172"
-            city="Dallas"
-            state="Texas"
-            zipCode={33172}
-            phone="555-01023021"
-          />
-
-          <ShippingAddress
-            name="John Doe"
-            address="Plaza Commerce St 172"
-            city="Dallas"
-            state="Texas"
-            zipCode={33172}
-            phone="555-01023021"
-          />
-
-          <ShippingAddress
-            name="John Doe"
-            address="Plaza Commerce St 172"
-            city="Dallas"
-            state="Texas"
-            zipCode={33172}
-            phone="555-01023021"
-          />
-
-          <ShippingAddress
-            name="John Doe"
-            address="Plaza Commerce St 172"
-            city="Dallas"
-            state="Texas"
-            zipCode={33172}
-            phone="555-01023021"
-          />
-        </form>
-        <UpdateForm />
-      </div>
+      <PersonalInfo edit={false} />
+      <AddressCont edit={false} />
     </>
   );
 }
