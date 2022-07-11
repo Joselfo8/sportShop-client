23e143f08edfe5ce987d5a367d460d923c9908e2
Hans Garcia <hansvekoni@gmail.com>
2022-07-11 15:21:33 -0300

    update validation model shopping_List

--- src/models/Stock.js
+++ src/models/Stock.js
@@ -6,6 +6,10 @@ module.exports = (sequelize) => {
     {
       size: {
         type: DataTypes.STRING,
+        validate: {
+          is: { arg: /^\S/, msg: "The size must not start with blank spaces" },
+          is: { arg: /^[a-zA-Z0-9-_]*$/, msg: "The size can only contain letters, numbers, hyphens and underscores" },     
+        }
       },
       cantidad: {
         type: DataTypes.INTEGER,
