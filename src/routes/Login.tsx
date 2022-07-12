6f7d41cbf6bcce8eebef873855e6c0cb781fc6a1
Andresser17 <tensasangetsu1254@gmail.com>
2022-07-12 16:29:54 -0400

    feat: add validation to Login form
    add react-hook-form, refactor Login and AddressEditor component

--- src/routes/Login.tsx
+++ src/routes/Login.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useForm } from "react-hook-form";
 // Components
 import Input from "../components/Input";
 // Icons
@@ -7,6 +7,11 @@ import { ReactComponent as GoogleIcon } from "../icons/google-icon.svg";
 import { ReactComponent as LinkedinIcon } from "../icons/linkedin-icon.svg";
 // Styles
 import styles from "./Login.module.css";
+// Validate inputs
+const userRegex = /^[a-zA-Z0-9_-]*$/g;
+const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+const passwordRegex =
+  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/g;
 
 function AuthLogin() {
   return (
@@ -33,30 +38,104 @@ function SubmitButton({
   );
 }
 
+interface SignUpInput {
+  username: string;
+  email: string;
+  password: string;
+  repeatPassword: string;
+}
+
 function SignUp() {
+  const { handleSubmit, control } = useForm<any>({
+    defaultValues: {
+      username: "",
+      email: "",
+      password: "",
+      repeatPassword: "",
+    },
+    mode: "onChange",
+  });
+  // send data to api
+  const onSubmit = (data: SignUpInput) => console.log(data);
+
   return (
-    <form>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <span className={styles["title"]}>Create account</span>
       <div className={styles["input-wrapper"]}>
-        <Input text="Username" id="username" getData={() => {}} />
-      </div>
-      <div className={styles["input-wrapper"]}>
-        <Input text="Email" id="email" getData={() => {}} />
-      </div>
-      <div className={styles["input-wrapper"]}>
         <Input
-          text="Password"
-          type="password"
-          id="password"
-          getData={() => {}}
+          control={control}
+          name="username"
+          label="Username"
+          rules={{
+            required: true,
+            minLength: {
+              value: 4,
+              message: "Username need a minimum of 4 characters",
+            },
+            maxLength: {
+              value: 16,
+              message: "Username can have 16 characters maximum",
+            },
+            pattern: {
+              value: userRegex,
+              message:
+                "Username can only include letters, numbers, dash and underscore",
+            },
+          }}
         />
       </div>
       <div className={styles["input-wrapper"]}>
         <Input
-          text="Repeat password"
+          control={control}
+          name="email"
+          label="Email"
+          rules={{
+            required: true,
+            pattern: {
+              value: emailRegex,
+              message: "Introduce a valid email address",
+            },
+          }}
+        />
+      </div>
+      <div className={styles["input-wrapper"]}>
+        <Input
+          control={control}
+          name="password"
+          label="Password"
           type="password"
-          id="repeat-password"
-          getData={() => {}}
+          rules={{
+            required: true,
+            maxLength: {
+              value: 20,
+              message: "Password can have 20 characters maximum",
+            },
+            pattern: {
+              value: passwordRegex,
+              message:
+                "Password should have minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character:",
+            },
+          }}
+        />
+      </div>
+      <div className={styles["input-wrapper"]}>
+        <Input
+          control={control}
+          name="repeatPassword"
+          label="Repeat Password"
+          type="password"
+          rules={{
+            required: true,
+            maxLength: {
+              value: 20,
+              message: "Password can have 20 characters maximum",
+            },
+            pattern: {
+              value: passwordRegex,
+              message:
+                "Password should have minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character:",
+            },
+          }}
         />
       </div>
       <div className={styles["button-cont"]}>
@@ -67,19 +146,48 @@ function SignUp() {
   );
 }
 
+interface SignInInput {
+  email: string;
+  password: string;
+}
+
 function SignIn() {
+  const { handleSubmit, control } = useForm<any>({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+    mode: "onChange",
+  });
+  // send data to api
+  const onSubmit = (data: SignInInput) => console.log(data);
+
   return (
-    <form>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <span className={styles["title"]}>Login</span>
       <div className={styles["input-wrapper"]}>
-        <Input text="Email" id="email" getData={() => {}} />
+        <Input
+          control={control}
+          name="email"
+          label="Email"
+          rules={{
+            required: true,
+            pattern: {
+              value: emailRegex,
+              message: "Introduce a valid email address",
+            },
+          }}
+        />
       </div>
       <div className={styles["input-wrapper"]}>
         <Input
-          text="Password"
+          control={control}
           type="password"
-          id="password"
-          getData={() => {}}
+          name="password"
+          label="Password"
+          rules={{
+            required: true,
+          }}
         />
         <span className={styles["subtitle"]}>Forgot your password?</span>
       </div>
