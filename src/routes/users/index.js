f043035174d0834e9b4cbd329270d83f9eda2a7d
Hans Garcia <hansvekoni@gmail.com>
2022-07-12 15:37:25 -0300

    autentication terminada falta protejer rutas

--- src/routes/users/index.js
+++ src/routes/users/index.js
@@ -17,6 +17,6 @@ router.delete("", deleteUser);
 
 router.post("/login", loginUser);// sorry but it most be a post not a get
 
-router.get("/:id", checkPermission , checkRoleUser('user'), getUser); // con checkRoleUser(['user']) ademas de tenee acceso a una secion tenga ahora el role de usuario
+router.get("/:id", checkPermission , checkRoleUser('admin'), getUser); // con checkRoleUser(['user']) ademas de tenee acceso a una secion tenga ahora el role de usuario
 
 module.exports = { users: router };
