5ee975a55db525dcaf104a9c615aa4587726f46d
DavidLBruno <bruno.david9914@gmail.com>
2022-07-13 10:07:07 -0300

    Lista de productos que vera el admin

--- src/App.tsx
+++ src/App.tsx
@@ -13,24 +13,29 @@ import UserProfile from "./routes/UserProfile";
 
 import About from "./components/About/About";
 import Favorites from "./components/Favorites/Favorites";
+import List from "./components/Admin/List/List";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        
+
         <Route path="/login" element={<Login />} />
         <Route path="/user/profile" element={<UserProfile />} />
-        
+
         <Route path="/cart" element={<Cart />} />
         <Route path="/favorites" element={<Favorites/>} />
 
         <Route path='/:category' element={<Subcategory/>} />
         <Route path="/products" element={<Products />} />
         <Route path="/products/:id" element={<Details />} />
-        
+
         <Route path="/about" element={<About />} />
+
+        {/* ADNIN */}
+        <Route path="/admin/list" element={<List />} />
+
       </Routes>
     </BrowserRouter>
   );
---
+++ src/components/Admin/List/List.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import NavBar from "../../Navbar/Navbar";
+import CardList from "./CardList";
+
+export default function(){
+    const dispatch = useDispatch();
+    const allProducts = useSelector((state:any) => state.products);
+
+    return(
+        <div>
+            <NavBar/>
+
+            {
+                allProducts.map(() => {
+                    return <CardList />
+                })
+            }
+
+        </div>
+    )
+}
