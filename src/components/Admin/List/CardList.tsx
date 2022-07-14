9bfcb1177274d9b2c7e1f5935d54ba58f2ab163c
DavidLBruno <bruno.david9914@gmail.com>
2022-07-14 09:16:58 -0300

    List terminada

--- src/components/Admin/List/CardList.tsx
+++ src/components/Admin/List/CardList.tsx
@@ -3,24 +3,33 @@ import { Link } from "react-router-dom";
 import styles from "./CardList.module.scss";
 import {FaTrash} from "react-icons/fa";
 import {BsFillPencilFill} from "react-icons/bs"
+import { useDispatch } from "react-redux";
+import { deleteProduct } from "../../../redux/action";
 
-export default function CardList(){
+export default function CardList({title, id, category, image}:any){
+    const dispatch = useDispatch();
+    const handleDelete = (id: number) => {
+        dispatch(deleteProduct(id));
+    };
     return(
         <div className={styles.bodyCard}>
-            <img src="" alt="" />
+
+            <div className={styles.containerImgInfo}>
+                <img src={image} alt="" style={{width:"70px"}}/>
 
             <div className={styles.containerInfo}>
-                <span>Title: {}</span>
-                <span>Id: {}</span>
-                <span>Category: {}</span>
-                <span>Creation date: {}</span>
+                    <span>Title: {title}</span>
+                    <span>Id: {id}</span>
+                    <span>Category: {category}</span>
+                    <span>Creation date:</span>
+                </div>
             </div>
 
             <div className={styles.containerButtons}>
-                <Link to="/edit" style={{color:"black"}}>
+                <Link to={`/edit/${id}`} style={{color:"black"}}>
                     <BsFillPencilFill/>
                 </Link>
-                <FaTrash/>
+                <FaTrash onClick={() => {handleDelete(id)}}/>
             </div>
 
         </div>
