1cd0f6e87bb315e469854578083177441d91b574
DavidLBruno <bruno.david9914@gmail.com>
2022-07-13 21:41:46 -0300

    ListAdmin

--- src/components/Admin/List/CardList.tsx
+++ src/components/Admin/List/CardList.tsx
@@ -1,23 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styles from "./CardList.module.scss";
+import {FaTrash} from "react-icons/fa";
+import {BsFillPencilFill} from "react-icons/bs"
 
 export default function CardList(){
     return(
         <div className={styles.bodyCard}>
-            <img />
+            <img src="" alt="" />
 
             <div className={styles.containerInfo}>
-                <span>Title</span>
-                <span>Id</span>
-                <span>Category</span>
-                <span>Creation date</span>
+                <span>Title: {}</span>
+                <span>Id: {}</span>
+                <span>Category: {}</span>
+                <span>Creation date: {}</span>
             </div>
 
             <div className={styles.containerButtons}>
-                <a href="/admin/edit"><button>LAPIZ</button></a>
-                <button>X</button>
+                <Link to="/edit" style={{color:"black"}}>
+                    <BsFillPencilFill/>
+                </Link>
+                <FaTrash/>
             </div>
+
         </div>
     );
 };
