4e0c4a2410acee30bc5cdca1aa1118789b283dd2
RRCRis <curiosity.cdrr@gmail.com>
2022-07-20 20:01:56 -0500

    finalized all changes in buys

--- src/routes/buys/functions.js
+++ src/routes/buys/functions.js
@@ -1,17 +1,28 @@
-const { Buy, User } = require("../../db");
+const { Buy, User, Product } = require("../../db");
 
 async function getBuys(req, res) {
-  let buys = await Buy.findAll({ include: [User] });
-  buys = buys.map((x) => {
-    return {
-      id: x.id,
-      status_history: x.status_history,
-      sub_total: x.sub_total,
-      user_id: x.user_id,
-      user: x.user.name,
-    };
-  });
-  res.send(buys);
+  try {
+    const { status } = req.query;
+    let buys = await Buy.findAll({ include: [User] });
+    if (status) {
+      buys = buys.filter(
+        (x) => x.status_history[x.status_history.length - 1].status === status
+      );
+    }
+    buys = buys.map((x) => {
+      return {
+        id: x.id,
+        status_history: x.status_history,
+        sub_total: x.sub_total,
+        user_id: x.user_id,
+        user: x.user.name,
+      };
+    });
+    res.send(buys);
+  } catch (error) {
+    console.log("error=>", error);
+    res.send({ msg: "failed to get buys", error });
+  }
 }
 async function getBuyById(req, res) {
   try {
@@ -46,9 +57,9 @@ async function postBuy(req, res) {
     const user = await User.findOne({ where: { id: id_user } });
     if (!user) return res.send({ msg: "user not found" });
 
-    const list = await user.getShopping_list();
-
-    let products = await list.getProducts();
+    const list = user.trolly;
+    let products = Object.keys(list);
+    products = await Product.findAll({ where: { id: products } });
     if (products.length === 0) return res.send({ msg: "list is empty" });
 
     products = products.map((x) => {
@@ -57,13 +68,14 @@ async function postBuy(req, res) {
         title: x.title,
         price: x.price,
         image: x.image,
+        sizesAmount: list[x.id],
       };
     });
 
     const suma = products.reduce((acc, cur) => acc + cur.price, 0);
 
-    const buy = await Buy.create({
-      status_history: [{ status: "pending", date: new Date() }],
+    let buy = await Buy.create({
+      status_history: [{ status: "create order", date: new Date() }],
       products: [...products],
       sub_total: suma,
       taxes: suma * 0.026,
@@ -75,7 +87,9 @@ async function postBuy(req, res) {
       country: country,
     });
     await user.addBuy(buy);
-    res.send(buy);
+    buy = await Buy.findOne({ where: { id: buy.id }, include: [User] });
+
+    res.send({ msg: "buy created", buy });
   } catch (error) {
     console.log("error=>", error);
     return res.send({ msg: "failed to add buy", error });
@@ -83,12 +97,26 @@ async function postBuy(req, res) {
 }
 
 async function putBuy(req, res) {
-  const { id, status } = req.body;
-  const buy = await Buy.findOne({ where: { id } });
-  buy.status_history = [...buy.status_history, { status, date: new Date() }];
-  await buy.save();
-  await buy.reload();
-  res.send(buy);
+  try {
+    const { id, status } = req.body;
+    //validations
+    if (!id) return res.send({ msg: "id is required" });
+    if (Number.isNaN(Number(id)))
+      return res.send({ msg: "id is not a number" });
+
+    if (!status) return res.send({ msg: "status is required" });
+
+    const buy = await Buy.findOne({ where: { id } });
+    if (!buy) return res.send({ msg: "buy not found" });
+
+    buy.status_history = [...buy.status_history, { status, date: new Date() }];
+    await buy.save();
+    await buy.reload();
+    res.send({ msg: "buy updated", status: buy.status_history });
+  } catch (error) {
+    console.log("error=>", error);
+    return res.send({ msg: "failed to update buy", error });
+  }
 }
 
 module.exports = {
