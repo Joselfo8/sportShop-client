a52dcc272a6eedbb9d5fbd274fab2b2711ac9871
RRCRis <curiosity.cdrr@gmail.com>
2022-07-27 16:08:15 -0500

    fix stripe

--- src/routes/stripes/index.js
+++ src/routes/stripes/index.js
@@ -1,31 +1,30 @@
 const router = require("express").Router();
-const Stripe = require('stripe') // Comunicarme con stripe
+const Stripe = require("stripe"); // Comunicarme con stripe
 
-const stripe = new Stripe("sk_test_51LKaEAATR7GdGLkc4i6xnMNGGjPnm6QnSt4NiLCJFWM3aQLUXPHVJNqOADyPjAmXr05o0pVIGVsckeotfJu9yxSr00awCEcsYt")
-const client_secret = "sk_test_51LMEEfF0EVRAGrurur4sJLK9rN6PfALbk7TH2iUaiqXBTW2KHKIaTiN8GKAGyw2HpEMcMwmAQM50EySNc3KXOi0C008qzaRa85";
+const stripe = new Stripe(
+  "sk_test_51LKaEAATR7GdGLkc4i6xnMNGGjPnm6QnSt4NiLCJFWM3aQLUXPHVJNqOADyPjAmXr05o0pVIGVsckeotfJu9yxSr00awCEcsYt"
+);
+const client_secret =
+  "sk_test_51LMEEfF0EVRAGrurur4sJLK9rN6PfALbk7TH2iUaiqXBTW2KHKIaTiN8GKAGyw2HpEMcMwmAQM50EySNc3KXOi0C008qzaRa85";
 
-router.post('/pay', async (req, res) => {
-    try{
-        const { id,jsonSoldProducts, amount } = req.body
-        console.log({id:id,jsonSoldProducts:jsonSoldProducts,amount:amount})
-        //res.send('ok')
-        const payment = await stripe.paymentIntents.create({
-            currency: "USD",
-            amount: amount,
-            description: `Sold products: ${jsonSoldProducts}`,
-            payment_method: id,
-            //cuantity: 1, //  xxx
-            // receipt_email: 'aurot122@gmail.com',
-            confirm: true
-        })
-        console.log(payment)
-        // Send publishable key and PaymentIntent details to client
-        res.send('Succesfull payment',payment.client_secret)
+router.post("/pay", async (req, res) => {
+  try {
+    const { id, jsonSoldProducts, amount } = req.body;
 
-    } catch(error) {
-        res.json({message: error.raw.message})
-        console.log(error)
-    }
-})
+    const payment = await stripe.paymentIntents.create({
+      currency: "USD",
+      amount: amount,
+      description: `Sold products: ${jsonSoldProducts}`,
+      payment_method: id,
+      confirm: true,
+    });
+    console.log(payment);
+    // Send publishable key and PaymentIntent details to client
+    res.send("Succesfull payment", payment.client_secret || payment.id);
+  } catch (error) {
+    res.json({ error });
+    console.log(error);
+  }
+});
 
 module.exports = { stripes: router };
